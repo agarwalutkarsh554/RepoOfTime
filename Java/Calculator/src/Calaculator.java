463c4c54046ed4effdcfc82b574af98bdc39fe5a
Sam W <twopizza9621536@gmail.com>
2020-12-04 11:00:40 -0500

    Update Calaculator.java

--- Java/Calculator/src/Calaculator.java
+++ Java/Calculator/src/Calaculator.java
@@ -8,44 +8,59 @@ public class Calaculator {
 		while (!endApp) {
 			String a = " ";
 			String b = " ";
-			char oper = ' ';
+			String oper = " ";
 			double result = 0.0;
 			Scanner input = new Scanner(System.in);
-			
+
 			System.out.println("Calaculator\nType in a number:");
 			a = input.nextLine();
-			
-			double cleana = 0.0;
-			
+
+			double cleana = tryParse(a);
+
 			System.out.println("Type in a operator:");
-			oper = input.next().charAt(0);
-			
+			oper = input.nextLine();
+
 			System.out.println("Type in a another number:");
 			b = input.nextLine();
-			
-			double cleanb = 0.0;
-			
-			input.close();
-			result = Calculate(cleana, oper, cleanb);
-			System.out.println(result);
+
+			double cleanb = tryParse(b);
+
+			try {
+				result = Calculate(cleana, oper, cleanb);
+				if (Double.isNaN(result))
+				{
+					System.out.println("This operation will result in a mathematical error.\n");
+				} else {
+				System.out.println(result);
+				}
+			} catch (Exception e) {
+				System.out.println("Oh no! An exception occurred trying to do the math.\n - Details: " + e.getMessage());
+			}
+
+			System.out.println("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+			if (input.nextLine() == "n") {
+				input.close();
+				endApp = true;
+			}
+			System.out.println("\n");
 		}
 	}
-	
-	private static double Calculate(double cleana, char oper, double cleanb) {
-		double result = 0.0;
+
+	private static double Calculate(double cleana, String oper, double cleanb) {
+		double result = Double.NaN;
 		switch (oper)
 		{
-		case '+':
+		case "+":
 			result = cleana + cleanb;
 			break;
-		case '-':
+		case "-":
 			result = cleana - cleanb;
 			break;
-		case '*':
+		case "*":
 			result = cleana * cleanb;
 			break;
-		case '/':
-			Scanner nonzero = new Scanner(System.in);				
+		case "/":
+			Scanner nonzero = new Scanner(System.in);
 			while (cleanb == 0) {
 				System.out.println("Enter a non-zero divisor: ");
 				cleanb = nonzero.nextInt();
@@ -56,5 +71,25 @@ public class Calaculator {
 			break;
 		}
 		return result;
+	}
+
+	private static double tryParse(String a)
+	{
+		double cleana = 0.0;
+		boolean endParse = false;
+		Scanner Parse = new Scanner(System.in);
+		
+		while (!endParse) {
+			try {
+				cleana = Double.parseDouble(a);
+				endParse = true;
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("\"This is not valid input. Please enter an integer value: \"");
+				a = Parse.nextLine();
+			}
+			Parse.close();
+		}
+		return cleana;
 	}
 }
