2cd36311899cad6b6905426f8cda0adb416371c8
Sam W <twopizza9621536@gmail.com>
2020-12-08 11:41:57 -0500

    Add a port from C# to C++

--- C++/Calculator/src/calculator.cpp
+++ C++/Calculator/src/calculator.cpp
@@ -1,19 +1,90 @@
-#include "calculator.h"
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <math.h>
 
-double Calculator::Calculate(double x, char oper, double y)
+double Calculate(double a, char oper, double b);
+
+int main()
 {
-    switch (oper)
+    bool endApp = 0;
+    while (!endApp)
     {
-        case '+':
-            return x + y;
-        case '-':
-            return x - y;
-        case '*':
-            return x * y;
-        case '/':
-            return x / y;
-        default:
-            return 0.0;
+        std::string a = "";
+        std::string b = "";
+        char oper = ' ';
+        std::string key = "";
+        double result = 0;
+
+        std::cout << "Calculator\nType a number:" << std::endl;
+        std::cin >> a;
+
+        double cleana = 0;
+
+        cleana = std::stod(a);
+
+        std::cout << "Type a operator:" << std::endl;
+        std::cin >> oper;
+
+        std::cout << "Type another number:" << std::endl;
+        std::cin >> b;
+
+        double cleanb = 0;
+
+        cleanb = std::stod(b);
+
+        try
+        {
+            result = Calculate(cleana, oper, cleanb);
+            if (isnan(result))
+            {
+                std::cout << "This operation will result in a mathematical error." << std::endl << std::endl;
+            }
+            else std::cout << "Your result: "<< result << std::endl << std::endl;
+        }
+        catch (_exception e)
+        {
+            std::cout << "Oh no! An exception occurred trying to do math." << std::endl << e.type << std::endl;
+            throw;
+        }
+
+        std::cout << "Press 'n' and Enter to close the app or press any other "
+                     "key and Enter to continue: ";
+        std::cin >> key;
+        if (key == "n")
+            endApp = 1;
+
+        std::cout << std::endl;
     }
 }
 
+double Calculate(double a, char oper, double b)
+{
+    double result = -NAN;
+    std::string newb = "";
+
+    switch ((int)oper)
+    {
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        while (b == 0)
+        {
+            std::cout << "Enter a non - zero divisor: " << std::endl;
+            std::cin >> newb;
+            b = std::stod(newb);
+        }
+        if (b != 0) result = a / b;
+        break;
+    default:
+        break;
+    }
+    return result;
+}
---
+++ Python/Calculator.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    print("Calculator\nType a number:")
+    a = input()
+
+    print("Type a operator:")
+    oper = input()
+
+    print("Type another number:")
+    b = input()
+
+
+
+result = 0.0
+def calculate(oper):
+    switcher = {
+        "+": result = a + b,
+    }
+    pass
